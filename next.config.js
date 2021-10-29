9163e3dd03df49a28c222d68627bcc8c7a1fad25
kenyk7 <kenykalin@gmail.com>
2021-10-29 10:25:29 -0500

    feat: Big bang + eslint-airbnb

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { dev }) => {
+    config.plugins.push(new ESLintPlugin({
+      emitWarning: dev,
+    }));
+    return config;
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../styles/globals.css';
+
+const propTypes = {
+  Component: PropTypes.any.isRequired,
+  pageProps: PropTypes.any.isRequired,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = propTypes;
+
+export default MyApp;
